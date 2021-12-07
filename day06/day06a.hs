f968c84bff45989a385e11c8543ad91cc8c3ed76
David Madouros <david.madouros@gmail.com>
2021-12-06 23:23:04 -0800

    End of day06

---
+++ day06/day06a.hs
@@ -0,0 +1,14 @@
+tick [a,b,c,d,e,f,g,h,i] = [b,c,d,e,f,g,h + a,i,a]
+
+play n = (!! n) . iterate tick
+
+count a = length . filter (== a)
+
+buckets = [0..8]
+
+main = do
+    input <- getContents
+    let fish = read $ "[" ++ input ++ "]" :: [Integer]
+    let histogram = map ((flip count) fish) $ buckets
+    
+    print . sum . play 80 $ histogram
