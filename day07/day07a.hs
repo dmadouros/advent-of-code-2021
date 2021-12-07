26a90d0665fb05f43879913d9b535b96c2a2a8fd
David Madouros <david.madouros@gmail.com>
2021-12-06 23:15:08 -0800

    End of day07

---
+++ day07/day07a.hs
@@ -0,0 +1,15 @@
+import Data.Text.Lazy(pack)
+
+
+main = do
+  contents <- getContents
+  let actualPositions = read $ "[" ++ contents ++ "]" :: [Integer]
+
+  let maxPosition = maximum actualPositions  
+  let minPosition = minimum actualPositions
+
+  let targetPositions = [minPosition..maxPosition]
+
+  let fuelBurned = minimum . map (\y -> sum . map (\x -> abs (y - x)) $ actualPositions) $ targetPositions
+
+  print fuelBurned
